793673e3ab9bd1d1177fcd2dca284a993bebcba9
gaetanlegac <contact@gaetan-legac.fr>
2026-03-21 16:43:56 +0800

    and deferred islands

---
+++ client/islands/index.ts
@@ -0,0 +1,7 @@
+export { default as useDeferredModule } from "./useDeferredModule";
+export type {
+  TDeferredModuleLoader,
+  TDeferredModuleOptions,
+  TDeferredModuleResult,
+  TDeferredModuleStrategy,
+} from "./useDeferredModule";
